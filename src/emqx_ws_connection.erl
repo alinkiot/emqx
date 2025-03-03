0698256e8cb15f5e65bf4bd5b6312ff6db11a87d
kenneth <529459515@qq.com>
2025-03-03 22:18:46 +0800

    Update emqx_ws_connection.erl

--- src/emqx_ws_connection.erl
+++ src/emqx_ws_connection.erl
@@ -766,12 +766,12 @@ classify([Packet|More], Packets, Cmds, Events)
     classify(More, [Packet|Packets], Cmds, Events);
 classify([Cmd = {active, _}|More], Packets, Cmds, Events) ->
     classify(More, Packets, [Cmd|Cmds], Events);
-classify([Cmd = {shutdown, _Reason}|More], Packets, Cmds, Events) ->
-    classify(More, Packets, [Cmd|Cmds], Events);
+classify([{shutdown, _Reason}|More], Packets, Cmds, Events) ->
+    classify(More, Packets, [close|Cmds], Events);
 classify([Cmd = close|More], Packets, Cmds, Events) ->
     classify(More, Packets, [Cmd|Cmds], Events);
 classify([Cmd = {close, _Reason}|More], Packets, Cmds, Events) ->
-    classify(More, Packets, [Cmd|Cmds], Events);
+    classify(More, Packets, [close|Cmds], Events);
 classify([Event|More], Packets, Cmds, Events) ->
     classify(More, Packets, Cmds, [Event|Events]).
 
